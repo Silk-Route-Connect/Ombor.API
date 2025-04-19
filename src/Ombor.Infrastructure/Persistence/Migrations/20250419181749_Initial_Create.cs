84c5738cdc26a15a819e0c5f068a17f39022ee3a
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-04-19 23:18:48 +0500

    initial migrations

---
+++ src/Ombor.Infrastructure/Persistence/Migrations/20250419181749_Initial_Create.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ombor.Infrastructure.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class Initial_Create : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Category",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Category", x => x.Id);
+            });
+
+        migrationBuilder.CreateTable(
+            name: "Product",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                SKU = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                Barcode = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                SalePrice = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                SupplyPrice = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                QuantityInStock = table.Column<int>(type: "int", nullable: false),
+                Measurement = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                CategoryId = table.Column<int>(type: "int", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Product", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_Product_Category_CategoryId",
+                    column: x => x.CategoryId,
+                    principalTable: "Category",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateIndex(
+            name: "IX_Product_CategoryId",
+            table: "Product",
+            column: "CategoryId");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Product");
+
+        migrationBuilder.DropTable(
+            name: "Category");
+    }
+}
