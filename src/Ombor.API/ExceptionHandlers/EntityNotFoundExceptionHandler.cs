04b7e5ad63c71e000e7904c4a778709b3d5f6d7f
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-04-20 18:04:49 +0500

    add error handlers

---
+++ src/Ombor.API/ExceptionHandlers/EntityNotFoundExceptionHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Ombor.Domain.Common;
+using Ombor.Domain.Exceptions;
+
+namespace Ombor.API.ExceptionHandlers;
+
+internal sealed class EntityNotFoundExceptionHandler(ILogger<EntityNotFoundExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is not EntityNotFoundException<EntityBase> entityNotFoundException)
+        {
+            return false;
+        }
+
+        var problemDetailsService = httpContext.RequestServices.GetRequiredService<IProblemDetailsService>();
+        var problemDetails = new ProblemDetails
+        {
+            Title = "Not Found",
+            Status = StatusCodes.Status404NotFound,
+            Detail = entityNotFoundException.Message,
+            Type = entityNotFoundException.ExceptionType,
+            Instance = httpContext.Request.Path
+        };
+
+        await problemDetailsService.WriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            ProblemDetails = problemDetails,
+            Exception = entityNotFoundException
+        });
+
+        logger.LogWarning(
+            exception,
+            "{Type} entity with Id: {Id} was not found.",
+            entityNotFoundException.EntityType,
+            entityNotFoundException.Id);
+
+        return true;
+    }
+}
---
+++ src/Ombor.API/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ombor.API.ExceptionHandlers;
+
+internal sealed class GlobalExceptionHandler(
+    ILogger<GlobalExceptionHandler> logger,
+    IWebHostEnvironment hostEnvironment,
+    IProblemDetailsService problemDetailsService) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Title = "Internal Server Error",
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = hostEnvironment.IsProduction() ? "An error occurred while processing the request." : exception.Message,
+            Type = "https://httpstatuses.com/500",
+            Instance = httpContext.Request.Path
+        };
+
+        await problemDetailsService.WriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            ProblemDetails = problemDetails,
+            Exception = exception,
+        });
+
+        logger.LogError(
+            exception,
+            "Unhandled exception processing {Method} {Path} (TraceId: {TraceId})",
+            httpContext.Request.Method,
+            httpContext.Request.Path,
+            httpContext.TraceIdentifier);
+
+        return true;
+    }
+}
