62fee776827adea95771a49b119ee4fb4589f33b
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-04-20 22:59:54 +0500

    add Application DI

---
+++ src/Ombor.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using Ombor.Application.Interfaces;
+using Ombor.Application.Services;
+
+namespace Ombor.Application.Extensions;
+
+public static class DependencyInjection
+{
+    private static Assembly CurrentAssembly => Assembly.GetExecutingAssembly();
+
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services
+            .AddFluentValidationAutoValidation()
+            .AddFluentValidationClientsideAdapters()
+            .AddValidatorsFromAssembly(CurrentAssembly);
+
+        services.AddScoped<IRequestValidator, RequestValidator>();
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<ICategoryService, CategoryService>();
+
+
+        return services;
+    }
+}
