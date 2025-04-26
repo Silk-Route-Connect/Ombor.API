f1732611120e50f3725a4f69cfc2b1350bd5d9e0
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-04-27 00:02:26 +0500

    update endpoint base class

--- src/Ombor.API/Program.cs
+++ src/Ombor.API/Program.cs
@@ -19,7 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseExceptionHandler(o => { });
+app.UseExceptionHandler(_ => { });
 
 app.UseHttpsRedirection();
 
--- tests/Ombor.Tests.Integration/Endpoints/EndpointTestsBase.cs
+++ tests/Ombor.Tests.Integration/Endpoints/EndpointTestsBase.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net.Http.Headers;
-using AutoFixture;
 using Bogus;
 using Ombor.Application.Interfaces;
 using Ombor.Tests.Integration.Helpers;
@@ -9,37 +8,20 @@ using Xunit.Abstractions;
 namespace Ombor.Tests.Integration.Endpoints;
 
 [Collection(nameof(DatabaseCollection))]
-public abstract class EndpointTestsBase : IClassFixture<TestingWebApplicationFactory>
+public abstract class EndpointTestsBase(TestingWebApplicationFactory factory, ITestOutputHelper outputHelper) : IClassFixture<TestingWebApplicationFactory>
 {
     protected const string NotFoundTitle = "Not Found";
 
-    protected readonly TestingWebApplicationFactory _factory;
-    protected readonly ITestOutputHelper _outputHelper;
-    protected readonly IApplicationDbContext _context;
-    protected readonly ResponseValidator _responseValidator;
-    protected readonly Fixture _fixture;
-    protected readonly Faker _faker;
-    protected readonly ApiClient _client;
-
-    protected EndpointTestsBase(TestingWebApplicationFactory factory, ITestOutputHelper outputHelper)
-    {
-        factory.ClientOptions.BaseAddress = new Uri("https://localhost/api/");
-        factory.ClientOptions.AllowAutoRedirect = false;
-
-        _factory = factory;
-        var client = factory.CreateClient();
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        _outputHelper = outputHelper;
-        _responseValidator = factory.ResponseValidator;
-        _context = factory.Context;
-        _fixture = CreateFixture();
-        _faker = new Faker();
-        _client = new ApiClient(client, outputHelper);
-    }
+    protected readonly ITestOutputHelper _outputHelper = outputHelper;
+    protected readonly IApplicationDbContext _context = factory.Context;
+    protected readonly ResponseValidator _responseValidator = factory.ResponseValidator;
+    protected readonly Faker _faker = new();
+    protected readonly ApiClient _client = CreateApiClient(factory, outputHelper);
 
     public static readonly IEnumerable<object[]> InvalidIds = [[-10], [-1], [0]];
 
     protected abstract string GetUrl();
+
     protected abstract string GetUrl(int id);
 
     protected string GetUrl<TRequest>(TRequest request)
@@ -62,13 +44,15 @@ public abstract class EndpointTestsBase : IClassFixture<TestingWebApplicationFac
     protected static string GetNotFoundErrorMessage(int id, string typeName)
         => $"{typeName} with ID {id} was not found.";
 
-    private static Fixture CreateFixture()
+    private static ApiClient CreateApiClient(TestingWebApplicationFactory factory, ITestOutputHelper outputHelper)
     {
-        var fixture = new Fixture();
-        fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
-        fixture.Behaviors.Add(new NullRecursionBehavior());
+        factory.ClientOptions.BaseAddress = new Uri("https://localhost/api/");
+        factory.ClientOptions.AllowAutoRedirect = false;
 
-        return fixture;
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        return new ApiClient(client, outputHelper);
     }
 
     protected static class Routes
