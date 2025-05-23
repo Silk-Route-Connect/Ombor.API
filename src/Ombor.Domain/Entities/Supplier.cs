b5714aa4936632f1b615b4462a9b1343f6cbc0ff
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-05-24 02:41:31 +0500

    created supplier's responses

--- src/Ombor.Domain/Entities/Supplier.cs
+++ src/Ombor.Domain/Entities/Supplier.cs
@@ -8,20 +8,20 @@ namespace Ombor.Domain.Entities;
 public class Supplier : EntityBase
 {
     /// <summary>Gets or sets the name of the supplier.</summary>
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
     /// <summary>Gets or sets the address of the supplier.</summary>
-    public string Address { get; set; }
+    public string? Address { get; set; }
 
     /// <summary>Gets or sets the email address of the supplier.</summary>
-    public string Email { get; set; }
+    public string? Email { get; set; }
 
     /// <summary>Gets or sets the name of the company associated with the supplier.</summary>
-    public string CompanyName { get; set; }
+    public string? CompanyName { get; set; }
 
     /// <summary>Gets or sets the supplier's status.</summary>
     public bool IsActive { get; set; }
 
     /// <summary>Gets or sets the supplier's phone numbers.</summary>
-    public List<string> PhoneNumbers { get; set; }
+    public List<string> PhoneNumbers { get; set; } = [];
 }
