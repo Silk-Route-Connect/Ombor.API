46e0c51f488ad96afcf14a4dbb64c1ff7b0490ac
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-05-27 23:41:39 +0500

    update product model to include image attachments

---
+++ src/Ombor.Domain/Entities/ProductImage.cs
@@ -0,0 +1,31 @@
+﻿using Ombor.Domain.Common;
+
+namespace Ombor.Domain.Entities;
+
+public class ProductImage : EntityBase
+{
+    /// <summary>
+    /// Gets or sets the URL of the product image.
+    /// </summary>
+    public required string ImageUrl { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the product image file.
+    /// </summary>
+    public required string ImageName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the thumbnail URL of the product image.
+    /// </summary>
+    public string? ThumnailUrl { get; set; }
+
+    /// <summary>
+    /// Gets or sets the product ID this image belongs to.
+    /// </summary>
+    public int ProductId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the product associated with this image.
+    /// </summary>
+    public required virtual Product Product { get; set; }
+}
