dba4a4ba63e263737467deecca000b3f91c7e5f0
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-06-01 00:58:36 +0500

    Create FileSettingsFactory.cs

---
+++ tests/Ombor.Tests.Common/Factories/FileSettingsFactory.cs
@@ -0,0 +1,21 @@
+﻿using Ombor.Application.Configurations;
+
+namespace Ombor.Tests.Common.Factories;
+
+public static class FileSettingsFactory
+{
+    public static FileSettings CreateDefault() =>
+        new()
+        {
+            BasePath = "uploads",
+            MaxBytes = 1000,
+            AllowedFileExtensions = [".png", ".jpg", ".pdf"],
+            AllowedImageExtensions = [".png", ".jpg"],
+            ThumbnailWidth = 100,
+            ThumbnailHeight = 100,
+            OriginalsSubfolder = "originals",
+            ThumbnailsSubfolder = "thumbnails",
+            ProductUploadsSection = "products",
+            PublicUrlPrefix = "images"
+        };
+}
