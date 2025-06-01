284e176cbc20487ca16dcc5283417a6e32c149d6
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-06-01 16:27:25 +0500

    Update IProductImageBuilder.cs

--- tests/Ombor.Tests.Common/Interfaces/IProductImageBuilder.cs
+++ tests/Ombor.Tests.Common/Interfaces/IProductImageBuilder.cs
@@ -9,6 +9,7 @@ public interface IProductImageBuilder
     IProductImageBuilder WithImageName(string? imageName = null);
     IProductImageBuilder WithOriginalUrl(string? originalUrl = null);
     IProductImageBuilder WithThumbnailUrl(string? thumbnailUrl = null);
+    IProductImageBuilder WithProductId(int? productId = null);
     IProductImageBuilder WithProduct(Product? product = null);
     ProductImage Build();
     ProductImage BuildAndPopulate();
