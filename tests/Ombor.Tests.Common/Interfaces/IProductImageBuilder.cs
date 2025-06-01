5be0db63232cd70b44ba1cee6b88c1eec6389895
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-06-01 14:10:21 +0500

    update interfaces

---
+++ tests/Ombor.Tests.Common/Interfaces/IProductImageBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Ombor.Domain.Entities;
+
+namespace Ombor.Tests.Common.Interfaces;
+
+public interface IProductImageBuilder
+{
+    IProductImageBuilder WithId(int? id = null);
+    IProductImageBuilder WithFileName(string? fileName = null);
+    IProductImageBuilder WithImageName(string? imageName = null);
+    IProductImageBuilder WithOriginalUrl(string? originalUrl = null);
+    IProductImageBuilder WithThumbnailUrl(string? thumbnailUrl = null);
+    IProductImageBuilder WithProduct(Product? product = null);
+    ProductImage Build();
+    ProductImage BuildAndPopulate();
+}
--- tests/Ombor.Tests.Common/Interfaces/ITestDataBuilder.cs
+++ tests/Ombor.Tests.Common/Interfaces/ITestDataBuilder.cs
@@ -4,4 +4,5 @@ public interface ITestDataBuilder
 {
     ICategoryBuilder CategoryBuilder { get; }
     IProductBuilder ProductBuilder { get; }
+    IProductImageBuilder ProductImageBuilder { get; }
 }
