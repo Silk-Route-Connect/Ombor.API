38334e687ba951b630c1a58bd50fc3bca53be7ed
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-06-09 22:07:53 +0500

    add validators for template

---
+++ src/Ombor.Application/Validators/Template/CreateTemplateRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Ombor.Contracts.Requests.Template;
+
+namespace Ombor.Application.Validators.Template;
+
+public sealed class CreateTemplateRequestValidator : AbstractValidator<CreateTemplateRequest>
+{
+    public CreateTemplateRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MaximumLength(ValidationConstants.DefaultStringLength)
+            .WithMessage($"Name must not exceed {ValidationConstants.DefaultStringLength} characters.");
+
+        RuleFor(x => x.Items)
+            .Must(x => x.Length > 0)
+            .WithMessage("Template items cannot be empty.");
+    }
+}
---
+++ src/Ombor.Application/Validators/Template/GetTemplateByIdRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Ombor.Contracts.Requests.Template;
+
+namespace Ombor.Application.Validators.Template;
+
+public sealed class GetTemplateByIdRequestValidator : AbstractValidator<GetTemplateByIdRequest>
+{
+    public GetTemplateByIdRequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0)
+            .WithMessage("Invalid Template ID.");
+    }
+}
