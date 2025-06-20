ed83c38b7b8d2f36ce3006ec9c0e8ce7cd0c652e
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-06-20 22:40:57 +0500

    Create EnumParseException.cs

---
+++ src/Ombor.Domain/Exceptions/EnumParseException.cs
@@ -0,0 +1,27 @@
+﻿namespace Ombor.Domain.Exceptions;
+
+public sealed class EnumParseException<TSource, TDestination> : Exception
+    where TSource : Enum
+    where TDestination : Enum
+{
+    public string SourceType { get; }
+    public string DestinationType { get; }
+
+    public EnumParseException() : base()
+    {
+        SourceType = typeof(TSource).Name;
+        DestinationType = typeof(TDestination).Name;
+    }
+
+    public EnumParseException(string? message) : base(message)
+    {
+        SourceType = typeof(TSource).Name;
+        DestinationType = typeof(TDestination).Name;
+    }
+
+    public EnumParseException(string? message, Exception? innerException) : base(message, innerException)
+    {
+        SourceType = typeof(TSource).Name;
+        DestinationType = typeof(TDestination).Name;
+    }
+}
