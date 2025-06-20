521a7b63790bd416ccf08288879b569d5f01cf06
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-06-20 22:40:40 +0500

    add helper extensions

---
+++ src/Ombor.Application/Extensions/PartnerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Ombor.Domain.Entities;
+using Ombor.Domain.Enums;
+
+namespace Ombor.Application.Extensions;
+
+internal static class PartnerExtensions
+{
+    public static void ApplyPayment(this Partner partner, Payment payment)
+    {
+        ArgumentNullException.ThrowIfNull(payment);
+
+        if (payment.Direction == PaymentDirection.Income)
+        {
+            partner.Balance += payment.AmountLocal;
+        }
+        else
+        {
+            partner.Balance -= payment.AmountLocal;
+        }
+    }
+
+    public static bool CanHandle(this PartnerType partnerType, Contracts.Enums.TransactionType transactionType) => transactionType switch
+    {
+        Contracts.Enums.TransactionType.Sale => partnerType is PartnerType.Customer or PartnerType.All,
+        Contracts.Enums.TransactionType.Supply => partnerType is PartnerType.Supplier or PartnerType.All,
+        Contracts.Enums.TransactionType.SaleRefund => partnerType is PartnerType.Customer or PartnerType.All,
+        Contracts.Enums.TransactionType.SupplyRefund => partnerType is PartnerType.Supplier or PartnerType.All,
+        _ => false
+    };
+}
