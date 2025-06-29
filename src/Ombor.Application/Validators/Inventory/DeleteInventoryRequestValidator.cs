7e99c558b64e18d0e09cb3eab4cace178858d6fa
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-06-30 00:51:01 +0500

    inventory mappings and validators

---
+++ src/Ombor.Application/Validators/Inventory/DeleteInventoryRequestValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+using Ombor.Contracts.Requests.Inventory;
+
+namespace Ombor.Application.Validators.Inventory;
+
+public sealed class DeleteInventoryRequestValidator : AbstractValidator<DeleteInventoryRequest>
+{
+    public DeleteInventoryRequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0)
+            .WithMessage("Invalid inventory ID.");
+    }
+}
