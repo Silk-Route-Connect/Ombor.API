bc10a2a3c95ccce9d1f44dda1c4e7902e1a83ad6
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-06-29 18:30:02 +0500

    add ledger interceptor

---
+++ src/Ombor.Infrastructure/Factories/LedgerEntryFactory.cs
@@ -0,0 +1,56 @@
+﻿using Ombor.Domain.Entities;
+using Ombor.Domain.Enums;
+
+namespace Ombor.Infrastructure.Factories;
+
+internal static class LedgerEntryFactory
+{
+    public static LedgerEntry FromTransaction(TransactionRecord transaction) => new()
+    {
+        CreatedAtUtc = transaction.DateUtc,
+        AmountLocal = GetAmount(transaction),
+        Notes = null,
+        Type = LedgerType.InvoiceCreated,
+        Source = nameof(TransactionRecord),
+        SourceId = transaction.Id,
+        PartnerId = transaction.PartnerId,
+        Partner = transaction.Partner
+    };
+
+    public static LedgerEntry FromPayment(Payment payment)
+    {
+        if (payment.PartnerId is null)
+        {
+            throw new InvalidOperationException("Cannot created ledger entry for payment without Partner.");
+        }
+
+        var entry = new LedgerEntry()
+        {
+            CreatedAtUtc = payment.DateUtc,
+            AmountLocal = GetAmount(payment),
+            Notes = null,
+            Type = LedgerType.InvoiceCreated,
+            Source = nameof(Payment),
+            SourceId = payment.Id,
+            PartnerId = payment.PartnerId.Value,
+            Partner = null! // will be set by EF
+        };
+
+        return entry;
+    }
+
+    private static decimal GetAmount(TransactionRecord transaction) =>
+        transaction.Type switch
+        {
+            TransactionType.Sale => -transaction.TotalDue,           // partner pays
+            TransactionType.Supply => +transaction.TotalDue,         // company pays
+            TransactionType.SaleRefund => +transaction.TotalDue,     // company pays out
+            TransactionType.SupplyRefund => -transaction.TotalDue,   // company gets refund
+            _ => 0m
+        };
+
+    private static decimal GetAmount(Payment payment) =>
+        payment.Direction == PaymentDirection.Income
+        ? -payment.AmountLocal  // partner pays
+        : +payment.AmountLocal; // company pays
+}
---
+++ src/Ombor.Infrastructure/Persistence/Interceptors/LedgerEntryInterceptor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Ombor.Domain.Entities;
+using Ombor.Infrastructure.Factories;
+
+namespace Ombor.Infrastructure.Persistence.Interceptors;
+
+internal sealed class LedgerEntryInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is ApplicationDbContext context)
+        {
+            AddTransactionLedgerEntries(context);
+            AddPaymentLedgerEntries(context);
+        }
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private static void AddTransactionLedgerEntries(ApplicationDbContext context)
+    {
+        var ledgerEntries = context.ChangeTracker
+            .Entries<TransactionRecord>()
+            .Where(x => x.State == EntityState.Added)
+            .Select(x => LedgerEntryFactory.FromTransaction(x.Entity))
+            .ToList();
+
+        context.LedgerEntries.AddRange(ledgerEntries);
+    }
+
+    private static void AddPaymentLedgerEntries(ApplicationDbContext context)
+    {
+        var paymentLedgerEntries = context.ChangeTracker
+            .Entries<Payment>()
+            .Where(x => x.State == EntityState.Added && x.Entity.PartnerId != null)
+            .Select(x => LedgerEntryFactory.FromPayment(x.Entity))
+            .ToList();
+
+        context.LedgerEntries.AddRange(paymentLedgerEntries);
+    }
+}
