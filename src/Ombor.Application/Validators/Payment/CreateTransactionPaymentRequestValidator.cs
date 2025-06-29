89082ca1f2469b426068c02f029ece90415aae7d
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-06-29 22:59:32 +0500

    add transaction payment contract models

---
+++ src/Ombor.Application/Validators/Payment/CreateTransactionPaymentRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Ombor.Contracts.Enums;
+using Ombor.Contracts.Requests.Payments;
+
+namespace Ombor.Application.Validators.Payment;
+
+public sealed class CreateTransactionPaymentRequestValidator : AbstractValidator<CreateTransactionPaymentRequest>
+{
+    public CreateTransactionPaymentRequestValidator()
+    {
+        RuleFor(x => x.TransactionId)
+            .NotNull()
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Invalid Transaction ID.");
+
+        RuleFor(x => x.Notes)
+            .MaximumLength(ValidationConstants.MaxStringLength)
+            .When(x => !string.IsNullOrEmpty(x.Notes));
+
+        RuleFor(x => x.Amount)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("Payment amount must be positive.");
+
+        RuleFor(x => x.ExchangeRate)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("Exchange rate must be positive")
+            .Equal(1)
+            .When(x => x.Currency == PaymentCurrency.USD)
+            .WithMessage("Exchange rate must 1 when payment is made in local currency.");
+    }
+}
