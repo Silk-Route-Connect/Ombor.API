6d1501908b9eab20099044fe3b3ba974d530cd0b
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-06-30 00:49:42 +0500

    inventory responses

---
+++ src/Ombor.Contracts/Responses/Inventory/CreateInventoryResponse.cs
@@ -0,0 +1,10 @@
+namespace Ombor.Contracts.Responses.Inventory;
+
+/// <summary>
+/// Response returned after successfully creating an inventory. 
+/// </summary>
+/// <param name="Id">The newly created inventory's ID.</param>
+/// <param name="Name">The inventory's name.</param>
+/// <param name="Location">The inventory's location, if any.</param>
+/// <param name="IsActive">The inventory's status.</param>
+public sealed record CreateInventoryResponse(int Id, string Name, string? Location, bool IsActive);
---
+++ src/Ombor.Contracts/Responses/Inventory/InventoryDto.cs
@@ -0,0 +1,10 @@
+namespace Ombor.Contracts.Responses.Inventory;
+
+/// <summary>
+/// DTO representing an inventory
+/// </summary>
+/// <param name="Id">The inventory ID.</param>
+/// <param name="Name">The inventory name.</param>
+/// <param name="Location">The inventory location if any.</param>
+/// <param name="IsActive">The inventory status.</param>
+public sealed record InventoryDto(int Id, string Name, string? Location, bool IsActive);
