7310b0f9341d8f22e0a54724302e7f64d85a3b0d
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-07-01 16:11:57 +0500

    add index on ledger entry source and source ID

---
+++ src/Ombor.Infrastructure/Persistence/Migrations/20250701051457_Index_Ledger_Source.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ombor.Infrastructure.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class Index_Ledger_Source : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateIndex(
+            name: "IX_LedgerEntry_SourceId_Source",
+            table: "LedgerEntry",
+            columns: new[] { "SourceId", "Source" },
+            unique: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "IX_LedgerEntry_SourceId_Source",
+            table: "LedgerEntry");
+    }
+}
