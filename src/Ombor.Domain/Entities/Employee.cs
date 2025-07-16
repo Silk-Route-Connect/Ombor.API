7aebde700d83baa412e90c820acc7601f8bc0f59
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-07-16 22:08:08 +0500

    added documentation to employee entity

--- src/Ombor.Domain/Entities/Employee.cs
+++ src/Ombor.Domain/Entities/Employee.cs
@@ -2,9 +2,23 @@ using Ombor.Domain.Common;
 
 namespace Ombor.Domain.Entities;
 
+/// <summary>
+/// Represents an employee in the system.
+/// </summary>
 public class Employee : AuditableEntity
 {
+    /// <summary>
+    /// Gets or sets the full name of the employee.
+    /// </summary>
     public required string FullName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the role associated with employee.
+    /// </summary>
     public required string Role { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the employee is active.
+    /// </summary>
     public bool IsActive { get; set; }
 }
