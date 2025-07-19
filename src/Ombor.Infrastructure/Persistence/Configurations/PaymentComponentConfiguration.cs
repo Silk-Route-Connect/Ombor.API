caf2cfa0c2a972180bc39c91ba3892cc0b4f815a
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-07-19 17:43:02 +0500

    add payment configurations

---
+++ src/Ombor.Infrastructure/Persistence/Configurations/PaymentComponentConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ombor.Domain.Entities;
+using Ombor.Infrastructure.Extensions;
+
+namespace Ombor.Infrastructure.Persistence.Configurations;
+
+internal sealed class PaymentComponentConfiguration : IEntityTypeConfiguration<PaymentComponent>
+{
+    public void Configure(EntityTypeBuilder<PaymentComponent> builder)
+    {
+        builder.ToTable(nameof(PaymentComponent));
+
+        builder.HasKey(pc => pc.Id);
+
+        builder
+            .HasOne(pc => pc.Payment)
+            .WithMany(p => p.Components)
+            .HasForeignKey(pc => pc.PaymentId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        builder
+            .Property(pc => pc.Amount)
+            .HasCurrencyPrecision()
+            .IsRequired();
+
+        builder
+            .Property(pc => pc.ExchangeRate)
+            .HasCurrencyPrecision()
+            .IsRequired();
+
+        builder
+            .Property(pc => pc.Currency)
+            .HasMaxLength(ConfigurationConstants.EnumLength)
+            .IsRequired();
+
+        builder
+            .Property(pc => pc.Method)
+            .HasConversion<string>()
+            .HasMaxLength(ConfigurationConstants.EnumLength)
+            .IsRequired();
+    }
+}
