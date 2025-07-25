5f327ff01cc1eddf19ba1245f25b66e5f3950046
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-07-25 22:26:09 +0500

    add transaction endpoints

---
+++ src/Ombor.API/Controllers/TransactionsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ombor.Application.Interfaces;
+using Ombor.Contracts.Requests.Transaction;
+using Ombor.Contracts.Responses.Transaction;
+
+namespace Ombor.API.Controllers;
+
+[Route("api/transactions")]
+[ApiController]
+public class TransactionsController(ITransactionService transactionService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<TransactionDto[]>> GetAsync(
+        [FromQuery] GetTransactionsRequest request)
+    {
+        var response = await transactionService.GetAsync(request);
+
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TransactionDto>> PostAsync(
+        [FromForm] CreateTransactionRequest request)
+    {
+        var response = await transactionService.CreateAsync(request);
+
+        return Created("", response);
+    }
+}
