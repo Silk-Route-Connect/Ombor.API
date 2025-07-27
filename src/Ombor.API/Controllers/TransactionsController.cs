acebb895ca7c400cbfe208ddf0fc4c6e2f6b55cb
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-07-27 14:11:11 +0500

    add payment endpoints

--- src/Ombor.API/Controllers/TransactionsController.cs
+++ src/Ombor.API/Controllers/TransactionsController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Ombor.Application.Interfaces;
+using Ombor.Contracts.Requests.Payment;
 using Ombor.Contracts.Requests.Transaction;
+using Ombor.Contracts.Responses.Payment;
 using Ombor.Contracts.Responses.Transaction;
 
 namespace Ombor.API.Controllers;
 
 [Route("api/transactions")]
 [ApiController]
-public class TransactionsController(ITransactionService transactionService) : ControllerBase
+public class TransactionsController(
+    ITransactionService transactionService,
+    IPaymentService paymentService) : ControllerBase
 {
     [HttpGet]
     public async Task<ActionResult<TransactionDto[]>> GetAsync(
@@ -16,6 +20,24 @@ public class TransactionsController(ITransactionService transactionService) : Co
         var response = await transactionService.GetAsync(request);
 
         return Ok(response);
+    }
+
+    [HttpGet("{id:int:min(1)}/payments")]
+    public async Task<ActionResult<TransactionPaymentDto[]>> GetPaymentsAsync(int id)
+    {
+        var request = new GetTransactionPaymentsRequest(id);
+        var response = await paymentService.GetTransactionPaymentsAsync(request);
+
+        return Ok(response);
+    }
+
+    [HttpGet("{id:int:min(1)}/lines")]
+    public async Task<ActionResult<TransactionLineDto[]>> GetLinesAsync(int id)
+    {
+        var request = new GetTransactionByIdRequest(id);
+        var response = await transactionService.GetByIdAsync(request);
+
+        return Ok(response.Lines);
     }
 
     [HttpPost]
