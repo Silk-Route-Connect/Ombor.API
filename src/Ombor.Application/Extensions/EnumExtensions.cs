f2255cd48ed8957f204950548df3c8fca6002372
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-07-27 14:05:07 +0500

    update helper extensions

--- src/Ombor.Application/Extensions/EnumExtensions.cs
+++ src/Ombor.Application/Extensions/EnumExtensions.cs
@@ -32,6 +32,16 @@ internal static class EnumExtensions
             _ => throw new InvalidCastException($"Could not map transaction type ({type}) to payment direction.")
         };
 
+    public static Domain.Enums.PaymentDirection GetPaymentDirection(this Domain.Enums.TransactionType type)
+        => type switch
+        {
+            Domain.Enums.TransactionType.Sale or
+            Domain.Enums.TransactionType.SupplyRefund => Domain.Enums.PaymentDirection.Income,
+            Domain.Enums.TransactionType.Supply or
+            Domain.Enums.TransactionType.SaleRefund => Domain.Enums.PaymentDirection.Expense,
+            _ => throw new InvalidCastException($"Could not map transaction type ({type}) to payment direction.")
+        };
+
     public static Domain.Enums.PaymentMethod ToDomainPaymentMethod(this Contracts.Enums.PaymentMethod method)
     {
         if (Enum.TryParse<Domain.Enums.PaymentMethod>(method.ToString(), ignoreCase: true, out var result))
@@ -50,5 +60,34 @@ internal static class EnumExtensions
         }
 
         throw new InvalidCastException($"Could not cast between payment allocation type and transaction type: {type}.");
+    }
+
+    public static Domain.Enums.PaymentType ToDomainType(this Contracts.Enums.PaymentType type)
+    {
+        if (Enum.TryParse<Domain.Enums.PaymentType>(type.ToString(), ignoreCase: true, out var result))
+        {
+            return result;
+        }
+
+        throw new InvalidCastException($"Could not cast between domain payment type and contract payment type: {type}.");
+    }
+
+    public static Domain.Enums.PaymentType ToDomainType(this Domain.Enums.PaymentAllocationType type)
+    {
+        if (Enum.TryParse<Domain.Enums.PaymentType>(type.ToString(), ignoreCase: true, out var result))
+        {
+            return result;
+        }
+        throw new InvalidCastException($"Could not cast between domain payment type and payment allocation type: {type}.");
+    }
+
+    public static Domain.Enums.PaymentDirection ToDomainDirection(this Contracts.Enums.PaymentDirection direction)
+    {
+        if (Enum.TryParse<Domain.Enums.PaymentDirection>(direction.ToString(), ignoreCase: true, out var result))
+        {
+            return result;
+        }
+
+        throw new InvalidCastException($"Could not cast between domain payment direction and contract payment direction: {direction}.");
     }
 }
---
+++ src/Ombor.Application/Extensions/RequestExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Ombor.Contracts.Requests.Payment;
+using Ombor.Contracts.Requests.Transaction;
+
+namespace Ombor.Application.Extensions;
+
+internal static class RequestExtensions
+{
+    public static CreateTransactionPaymentRequest ToPaymentRequest(this CreateTransactionRequest request, int transactionId)
+    {
+        return new CreateTransactionPaymentRequest(
+            transactionId,
+            request.Notes,
+            request.ShouldReturnChange,
+            request.Attachments,
+            request.Payments,
+            request.DebtPayments);
+    }
+}
