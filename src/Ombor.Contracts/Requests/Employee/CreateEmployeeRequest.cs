f05b32061a9bdb090db89f3c13d2d1a5abb4c5d2
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-08-03 14:07:36 +0500

    employee requests

--- src/Ombor.Contracts/Requests/Employee/CreateEmployeeRequest.cs
+++ src/Ombor.Contracts/Requests/Employee/CreateEmployeeRequest.cs
@@ -1,3 +1,15 @@
+using Ombor.Contracts.Enums;
+
 namespace Ombor.Contracts.Requests.Employee;
 
-public sealed record CreateEmployeeRequest(string FullName, string Role);
+public sealed record CreateEmployeeRequest(
+    string FullName,
+    decimal Salary,
+    string PhoneNumber,
+    string Email,
+    string? Address,
+    string? Description,
+    EmployeeRole Role,
+    EmployeeAccess Access,
+    DateTime DateOfEmployment
+    );
--- src/Ombor.Contracts/Requests/Employee/UpdateEmployeeRequest.cs
+++ src/Ombor.Contracts/Requests/Employee/UpdateEmployeeRequest.cs
@@ -1,7 +1,17 @@
+using Ombor.Contracts.Enums;
+
 namespace Ombor.Contracts.Requests.Employee;
 
 public sealed record UpdateEmployeeRequest(
     int Id,
     string FullName,
-    string Role,
-    bool IsActive);
+    decimal Salary,
+    string PhoneNumber,
+    string Email,
+    string? Address,
+    string? Description,
+    EmployeeRole Role,
+    EmployeeAccess Access,
+    EmployeeStatus Status,
+    DateTime DateOfEmployment
+    );
