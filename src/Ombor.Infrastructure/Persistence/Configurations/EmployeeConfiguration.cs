cff147ccc38bea843af758a0a824403de70f3ff4
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-08-03 14:07:15 +0500

    Update EmployeeConfiguration.cs

--- src/Ombor.Infrastructure/Persistence/Configurations/EmployeeConfiguration.cs
+++ src/Ombor.Infrastructure/Persistence/Configurations/EmployeeConfiguration.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Ombor.Domain.Entities;
 
 namespace Ombor.Infrastructure.Persistence.Configurations;
+
 internal sealed class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
 {
     public void Configure(EntityTypeBuilder<Employee> builder)
@@ -20,5 +21,27 @@ internal sealed class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
             .Property(e => e.Role)
             .HasMaxLength(ConfigurationConstants.DefaultStringLength)
             .IsRequired();
+
+        builder
+            .Property(e => e.Salary)
+            .IsRequired();
+
+        builder
+            .Property(e => e.Description)
+            .HasMaxLength(ConfigurationConstants.MaxStringLength)
+            .IsRequired(false);
+
+        builder
+            .Property(e => e.Address)
+            .HasMaxLength(ConfigurationConstants.MaxStringLength)
+            .IsRequired(false);
+
+        builder
+            .Property(e => e.Email)
+            .HasMaxLength(ConfigurationConstants.DefaultStringLength);
+
+        builder
+            .Property(e => e.PhoneNumber)
+            .HasMaxLength(ConfigurationConstants.DefaultStringLength);
     }
 }
