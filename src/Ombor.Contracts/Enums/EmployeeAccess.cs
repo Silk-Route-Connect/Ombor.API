c52dd0389022bcb16800fb56337bec4c4de271bc
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-08-11 11:08:25 +0500

    employee access enum

--- src/Ombor.Contracts/Enums/EmployeeAccess.cs
+++ src/Ombor.Contracts/Enums/EmployeeAccess.cs
@@ -1,5 +1,28 @@
 ﻿namespace Ombor.Contracts.Enums;
 public enum EmployeeAccess
 {
-    None = 0,
+    /// <summary>
+    /// Can manage sales and close customer debts.
+    /// </summary>
+    SalesAccess = 1,
+
+    /// <summary>
+    /// Can manage supplies and settle supplier debts.
+    /// </summary>
+    SuppliesAccess = 2,
+
+    /// <summary>
+    /// Can view and close customer debts.
+    /// </summary>
+    CustomerDebtsAccess = 3,
+
+    /// <summary>
+    /// Can View and close supplier debts.
+    /// </summary>
+    SupplierDebtsAccess = 4,
+
+    /// <summary>
+    /// Full access to all application features.
+    /// </summary>
+    FullAccess = 5,
 }
