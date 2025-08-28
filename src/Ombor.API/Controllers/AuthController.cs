046de89e93364aa496cda1015af5acc9f4125920
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-08-28 17:28:12 +0500

    Create AuthController.cs

---
+++ src/Ombor.API/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ombor.Application.Interfaces;
+using Ombor.Contracts.Requests.Auth;
+
+namespace Ombor.API.Controllers;
+
+[Route("api/auth")]
+[ApiController]
+public class AuthController(IAuthService service) : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult> RegisterAsync([FromBody] RegisterRequest request)
+    {
+        await service.RegisterAsync(request);
+
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> LoginAsync([FromBody] LoginRequest request)
+    {
+        var response = await service.LoginAsync(request);
+
+        return Ok();
+    }
+}
