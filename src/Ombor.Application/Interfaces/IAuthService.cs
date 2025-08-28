1ae186661e1129c617f8e92c2e8fd4d1ef55a645
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-08-28 17:30:20 +0500

    Create IAuthService.cs

---
+++ src/Ombor.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Ombor.Contracts.Requests.Auth;
+using Ombor.Contracts.Responses.Auth;
+
+namespace Ombor.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task RegisterAsync(RegisterRequest request);
+    Task<LoginResponse> LoginAsync(LoginRequest request);
+}
