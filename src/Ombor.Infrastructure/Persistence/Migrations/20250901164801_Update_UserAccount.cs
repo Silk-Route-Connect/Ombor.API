49af6e6514877fb20bffc3a3fb8955832ed66867
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-09-01 22:07:59 +0500

    migrations

---
+++ src/Ombor.Infrastructure/Persistence/Migrations/20250901164801_Update_UserAccount.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ombor.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_UserAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "UserAccount",
+                newName: "TelegramAccount");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "UserAccount",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "UserAccount",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "UserAccount");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "UserAccount");
+
+            migrationBuilder.RenameColumn(
+                name: "TelegramAccount",
+                table: "UserAccount",
+                newName: "FullName");
+        }
+    }
+}
