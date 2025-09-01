3bfddbb10ebaa832610ecd1e1063c14ec9c58830
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-09-01 22:10:52 +0500

    Create TokenHandlerService.cs

---
+++ src/Ombor.Application/Services/TokenHandlerService.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Ombor.Application.Interfaces;
+using Ombor.Domain.Entities;
+
+namespace Ombor.Application.Services;
+
+internal class TokenHandlerService(IConfiguration configuration) : ITokenHandlerService
+{
+    public string GenerateVerificationToken(UserAccount user, string code)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub,user.Id.ToString()),
+            new Claim("UserId",user.Id.ToString()),
+            new Claim("VerificationCode",code),
+            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["Jwt:Issuer"],
+            audience: configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(5),
+            signingCredentials: credentials
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
