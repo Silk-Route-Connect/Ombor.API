efd72d79ba3b9369ecef4df3d1bbde8bbe4b712a
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-09-06 23:41:40 +0500

    add packaging to product

---
+++ src/Ombor.Domain/Entities/ProductPackaging.cs
@@ -0,0 +1,8 @@
+﻿namespace Ombor.Domain.Entities;
+
+public sealed class ProductPackaging
+{
+    public int Size { get; set; }
+    public string? Label { get; set; }
+    public string? Barcode { get; set; }
+}
