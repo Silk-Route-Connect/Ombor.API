9132efa11af6637c0a72fad53af8c5862a52370d
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-10-04 18:01:04 +0500

    fix auth related bugs

---
+++ src/Ombor.Infrastructure/Services/MemoryCache.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Ombor.Application.Interfaces;
+
+namespace Ombor.Infrastructure.Services;
+
+internal sealed class MemoryCache(IMemoryCache cache) : IRedisService
+{
+    public Task<T?> GetAsync<T>(string key)
+        => Task.FromResult(cache.Get<T>(key));
+
+    public Task RemoveAsync(string key)
+    {
+        cache.Remove(key);
+
+        return Task.CompletedTask;
+    }
+
+    public Task<bool> SetAsync<T>(string key, T value, TimeSpan? expiry = null)
+    {
+        cache.GetOrCreateAsync(key, entry =>
+        {
+            if (expiry.HasValue)
+            {
+                entry.SetAbsoluteExpiration(expiry.Value);
+            }
+
+            return Task.FromResult(value);
+        });
+
+        return Task.FromResult(true);
+    }
+}
