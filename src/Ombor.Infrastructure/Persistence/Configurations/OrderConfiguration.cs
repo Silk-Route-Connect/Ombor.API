458002f0754476ccd86f0a666f9928c36b2fb9f7
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-10-11 20:36:53 +0500

    add EF configurations

---
+++ src/Ombor.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ombor.Domain.Entities;
+using Ombor.Infrastructure.Extensions;
+
+namespace Ombor.Infrastructure.Persistence.Configurations;
+
+internal sealed class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.ToTable(nameof(Order));
+
+        builder.HasKey(o => o.Id);
+
+        builder.HasMany(o => o.Lines)
+            .WithOne(ol => ol.Order)
+            .HasForeignKey(o => o.OrderId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        builder.HasOne(o => o.Customer)
+            .WithMany(c => c.Orders)
+            .HasForeignKey(o => o.CustomerId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        builder
+            .Property(o => o.OrderNumber)
+            .HasMaxLength(ConfigurationConstants.EnumLength)
+            .IsRequired();
+
+        builder
+            .Property(o => o.TotalAmount)
+            .HasCurrencyPrecision()
+            .IsRequired();
+
+        builder
+            .Property(o => o.DateUtc)
+            .IsRequired();
+
+        builder
+            .Property(o => o.Status)
+            .HasEnumConversion()
+            .IsRequired();
+
+        builder
+            .Property(o => o.Source)
+            .HasEnumConversion()
+            .IsRequired();
+    }
+}
---
+++ src/Ombor.Infrastructure/Persistence/Configurations/OrderLineConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ombor.Domain.Entities;
+using Ombor.Infrastructure.Extensions;
+
+namespace Ombor.Infrastructure.Persistence.Configurations;
+
+internal sealed class OrderLineConfiguration : IEntityTypeConfiguration<OrderLine>
+{
+    public void Configure(EntityTypeBuilder<OrderLine> builder)
+    {
+        builder.ToTable(nameof(OrderLine));
+
+        builder.HasKey(ol => ol.Id);
+
+        builder.HasOne(ol => ol.Order)
+            .WithMany(o => o.Lines)
+            .HasForeignKey(ol => ol.OrderId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        builder.HasOne(ol => ol.Product)
+            .WithMany(p => p.OrderLines)
+            .HasForeignKey(ol => ol.ProductId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        builder
+            .Property(ol => ol.Quantity)
+            .IsRequired();
+
+        builder
+            .Property(ol => ol.UnitPrice)
+            .HasCurrencyPrecision()
+            .IsRequired();
+
+        builder
+            .Property(ol => ol.Discount)
+            .HasCurrencyPrecision()
+            .IsRequired(false);
+    }
+}
