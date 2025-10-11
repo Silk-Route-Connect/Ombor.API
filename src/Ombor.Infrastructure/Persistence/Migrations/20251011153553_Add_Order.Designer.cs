b572244496111cb0186a030bf6f15410c355ab8b
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-10-11 20:37:29 +0500

    add migration

--- src/Ombor.Infrastructure/Persistence/Migrations/20251011153553_Add_Order.Designer.cs
+++ src/Ombor.Infrastructure/Persistence/Migrations/20251011153553_Add_Order.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ombor.Infrastructure.Persistence;
 
@@ -12,9 +13,11 @@ using Ombor.Infrastructure.Persistence;
 namespace Ombor.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251011153553_Add_Order")]
+    partial class Add_Order
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Ombor.Infrastructure/Persistence/Migrations/20251011153553_Add_Order.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ombor.Infrastructure.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class Add_Order : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Order",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                OrderNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                TotalAmount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                DateUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                Status = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                Source = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                CustomerId = table.Column<int>(type: "int", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Order", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_Order_Partner_CustomerId",
+                    column: x => x.CustomerId,
+                    principalTable: "Partner",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateTable(
+            name: "OrderLine",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Quantity = table.Column<int>(type: "int", nullable: false),
+                UnitPrice = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                Discount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: true),
+                OrderId = table.Column<int>(type: "int", nullable: false),
+                ProductId = table.Column<int>(type: "int", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_OrderLine", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_OrderLine_Order_OrderId",
+                    column: x => x.OrderId,
+                    principalTable: "Order",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_OrderLine_Product_ProductId",
+                    column: x => x.ProductId,
+                    principalTable: "Product",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateIndex(
+            name: "IX_Order_CustomerId",
+            table: "Order",
+            column: "CustomerId");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_OrderLine_OrderId",
+            table: "OrderLine",
+            column: "OrderId");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_OrderLine_ProductId",
+            table: "OrderLine",
+            column: "ProductId");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "OrderLine");
+
+        migrationBuilder.DropTable(
+            name: "Order");
+    }
+}
