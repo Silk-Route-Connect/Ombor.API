a575de27f056af22dfb10031e93216bf7745bb80
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-10-11 22:41:19 +0500

    implement request validators

---
+++ src/Ombor.Application/Validators/Order/CreateOrderRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Ombor.Contracts.Requests.Order;
+
+namespace Ombor.Application.Validators.Order;
+
+public sealed class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequest>
+{
+    public CreateOrderRequestValidator()
+    {
+        RuleFor(x => x.Source)
+            .IsInEnum()
+            .WithMessage("Invalid order source.");
+
+        RuleFor(x => x.DeliveryAddress)
+            .NotNull()
+            .WithMessage("Delivery address is required.");
+
+        RuleFor(x => x.Lines)
+            .NotEmpty()
+            .WithMessage("At least one order line is required.")
+            .Must(lines => lines.All(line => line.Quantity > 0))
+            .WithMessage("All order lines must have a quantity greater than zero.")
+            .Must(lines => lines.All(line => line.UnitPrice > 0))
+            .WithMessage("All order lines must have a unit price greater than zero.")
+            .Must(lines => lines.All(lines => lines.UnitPrice >= lines.Discount))
+            .WithMessage("All order lines must have a unit price greater than discount.");
+    }
+}
---
+++ src/Ombor.Application/Validators/Order/GetOrdersRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Ombor.Contracts.Requests.Order;
+
+namespace Ombor.Application.Validators.Order;
+
+public sealed class GetOrdersRequestValidator : AbstractValidator<GetOrdersRequest>
+{
+    public GetOrdersRequestValidator()
+    {
+        RuleFor(x => x.SearchTerm)
+            .MaximumLength(ValidationConstants.DefaultStringLength)
+            .WithMessage($"Search term must not exceed {ValidationConstants.DefaultStringLength} characters.");
+
+        RuleFor(x => x.PartnerId)
+            .GreaterThan(0)
+            .When(x => x.PartnerId.HasValue)
+            .WithMessage(x => $"Invalid partner ID: {x.PartnerId}.");
+
+        RuleFor(x => x.FromDate)
+            .LessThanOrEqualTo(x => x.ToDate)
+            .When(x => x.FromDate.HasValue && x.ToDate.HasValue)
+            .WithMessage("'From date' must be less than or equal to 'to date'.");
+    }
+}
