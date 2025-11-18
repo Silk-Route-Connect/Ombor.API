69bdefc355010c8e81349610ed3381cce0588e60
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-11-18 12:33:40 +0500

    fix tests

--- src/Ombor.TestDataGenerator/Generators/InventoryItemGenerator.cs
+++ src/Ombor.TestDataGenerator/Generators/InventoryItemGenerator.cs
@@ -10,14 +10,32 @@ public static class InventoryItemGenerator
         GetGenerator([productId], [inventoryId]).Generate();
 
     public static List<InventoryItem> Generate(int productId, int inventoryId, int itemsCount) =>
-    GetGenerator([productId], [inventoryId]).Generate(itemsCount);
+        GetGenerator([productId], [inventoryId]).Generate(itemsCount);
 
     private static Faker<InventoryItem> GetGenerator(
         int[] productIds,
         int[] inventoryIds,
-        string? loacle = DefaultLocale)
-        => new Faker<InventoryItem>(loacle)
-        .RuleFor(x => x.Quantity, f => f.Random.Number(1, 100))
-        .RuleFor(x => x.ProductId, f => f.PickRandom(productIds))
-        .RuleFor(x => x.InventoryId, f => f.PickRandom(inventoryIds));
+        string? locale = DefaultLocale)
+    {
+        var products = ProductGenerator.Generate(10, productIds.Length);
+
+        products = EnsureProductsMatch(productIds, products);
+
+        return new Faker<InventoryItem>(locale)
+            .RuleFor(x => x.Quantity, f => f.Random.Number(1, 100))
+            .RuleFor(x => x.ProductId, f => f.PickRandom(productIds))
+            .RuleFor(x => x.Product, f => f.PickRandom(products))
+            .RuleFor(x => x.InventoryId, f => f.PickRandom(inventoryIds));
+    }
+
+    private static List<Product> EnsureProductsMatch(int[] productIds, List<Product> products)
+    {
+        for (int i = 0; i < products.Count; i++)
+        {
+            products[i].Id = productIds[i];
+        }
+
+        return products;
+    }
+
 }
